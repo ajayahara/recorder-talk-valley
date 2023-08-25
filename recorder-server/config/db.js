25174368ea1b39116bcfa871bf7c8a44e1b55f44
ajayahara <abak00350@gmail.com>
2023-08-25 14:12:28 +0530

    authentication completed

--- recorder-server/config/db.js
+++ recorder-server/config/db.js
@@ -1,3 +1,4 @@
 const mongoose=require("mongoose");
-const connection="";
+const connection=mongoose.connect(`mongodb+srv://abak00350:talk-valley-recorder@cluster0.ysbocax.mongodb.net/RecordUser?retryWrites=true&w=majority`);
+module.exports={connection};
 
--- recorder-server/index.js
+++ recorder-server/index.js
@@ -1,5 +1,57 @@
+require('dotenv').config();
 const express=require("express");
+const cors=require("cors");
+const jwt=require("jsonwebtoken")
+const { connection } = require("./config/db.js");
+const { UserModel } = require('./schema/user.model.js');
 const app=express();
-app.listen(5050,async ()=>{
-    console.log("server is running at port 5050")
+app.use(cors())
+app.use(express.json());
+app.get("/",async (req,res)=>{
+    const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGU4NjMzMTgxYmU2NTdhMzhkNzhkYjIiLCJpYXQiOjE2OTI5NTE5MjJ9.NsvD8fjj4vZmWKQDcQ7zZaa7xy9prncaAsLRE2yVRsM"
+    const result=jwt.verify(token,process.env.PRIVATE_KEY);
+    console.log(new Date(result.iat));
+    try {
+        res.json({"msg":"You are at home"})
+    } catch (err) {
+       res.send({err})
+    }
+})
+app.post("/user/resister",async (req,res)=>{
+    const {name,email,password}=req.body;
+    try {
+        const existingusers=await UserModel.find({email});
+        if(existingusers.length){
+            res.json({ok:true,msg:"User Already Exit"})
+        }else{
+            const newUser=new UserModel({name,email,password});
+            await newUser.save();
+            res.json({ok:true,msg:"Registration Successful"})
+        }
+    } catch (err) {
+       res.json({ok:false,msg:"An Error Occured"})
+    }
+})
+app.post("/user/login",async (req,res)=>{
+    const {name,email,password}=req.body;
+    try {
+        const existingusers=await UserModel.find({email,password});
+        if(!existingusers.length){
+            res.json({ok:true,msg:"No User Found"})
+        }else{
+            const token=jwt.sign({userId:existingusers[0]._id},process.env.PRIVATE_KEY);
+            res.json({ok:true,msg:"Login Successful",token})
+        }
+    } catch (err) {
+        console.log(err)
+        res.json({ok:false,msg:"An Error Occured"}) 
+    }
+})
+app.listen(process.env.PORT,async ()=>{
+    try {
+        await connection;
+        console.log(`server is running at port ${process.env.PORT}`)
+    } catch (err) {
+        console.log(err)
+    }
 })
